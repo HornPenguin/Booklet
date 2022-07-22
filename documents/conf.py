7edece70359341aa550234679696e6e9ace7bd5e
Kim, Hyunseong <qwqwhsnote@gm.gist.ac.kr>
2022-07-22 13:07:16 +0900

    Update conf.py

--- documents/conf.py
+++ documents/conf.py
@@ -55,7 +55,10 @@ exclude_patterns = []
 html_permalinks_icon = '<span>#</span>'
 html_theme = 'sphinxawesome_theme'
 
+html_theme_options = {
+    "extra_header_links" : {"Docs": "https:://docs.hornpenguin.com", "출판사":"https:://www.hornpenguin.com"}
 
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
